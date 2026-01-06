ca8de09e0eb889964dc1fd0ea76217b696558cf4
chandarhema <chandarhema145@gmail.com>
2026-01-06 16:33:34 +0530

    msg

--- lab_2/question_1.py
+++ lab_2/question_1.py
@@ -2,13 +2,4 @@
 #
 # Compute the covariance matrix using matrix multiplications.
 # Verify your results by using numpy library operations
-# Implement california housing
-# prediction model using scikit-learn - walkthro’ of bdbp207_californiahousing.py
-# Implement california housing
-# prediction model using scikit-learn - walkthro’ of bdbp207_californiahousing.py
-# Implement california housing
-# prediction model using scikit-learn - walkthro’ of bdbp207_californiahousing.py
-# Implement california housing
-# prediction model using scikit-learn - walkthro’ of bdbp207_californiahousing.py
-# Implement california housing
-# prediction model using scikit-learn - walkthro’ of bdbp207_californiahousing.py
+
