d2fc2b11d2f95c87b6c375c39b5bc140ec7af1a7
chandarhema <chandarhema145@gmail.com>
2026-03-16 14:24:21 +0530

    lab13

--- lab_8/question 3.py
+++ lab_8/question 3.py
@@ -1,3 +1,28 @@
 """Implement ordinal encoding and one-hot encoding methods in Python from scratch.
 """
 
+
+def ordinal_encoding(data):
+    unique = []
+    for item in data:
+        if item not in unique:
+            unique.append(item)
+    print(unique)
+    mapping = {value: i for i, value in enumerate(unique)}
+    encoded = [mapping[item] for item in data]
+    print(encoded)
+    print(mapping)
+
+def onehot_encoding(data):
+    
+
+def main():
+    data = ["red","blue","green","white","black","pink","purple","yellow","pink","blue","green","white","black","pink"]
+    ordinal_encoding(data)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
